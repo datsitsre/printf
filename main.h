bca5f58cf3bd41c2e846d062c9a68a9a4eff8bbf
datsitsre <atsitsredaniel@gmail.com>
2023-01-25 00:06:37 +0000

    get_flag.c

--- main.h
+++ main.h
@@ -12,8 +12,8 @@
   */
 typedef struct flags
 {
-	int space;
 	int plus;
+	int space;
 	int hash;
 } flags_t;
 
@@ -49,7 +49,7 @@ int count_digit(int i);
 char *convert(unsigned long int num, int base, int lowercase);
 
 /* get print */
-int (*get_print(char s))(va_list l, flags_t *);
+int (*get_print(char s))(va_list l, flags_t *f);
 
 /* get flag */
 int get_flag(char s, flags_t *f);
